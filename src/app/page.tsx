59bdc28587b40c2c83df8482f03e86c8f59fee67
Ben McAdams <mcadams.benj@gmail.com>
2024-04-16 13:52:47 -0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import dynamic from 'next/dynamic'
+const DynamicMapComponent = dynamic(() => import('./components/MapComponent'), {
+  ssr: false,
+})
+
+const MyPage = () => {
+  return (
+    <div>
+      <DynamicMapComponent />
+    </div>
+  )
+}
+
+export default MyPage
