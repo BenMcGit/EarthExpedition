9ab3fea7a2a606938c4ac5c2ecd81491bd5312d8
Ben McAdams <mcadams.benj@gmail.com>
2024-04-16 17:37:40 -0700

    fix: move to individual components

---
+++ src/app/components/ZoomHandler.tsx
@@ -0,0 +1,39 @@
+import { FC, useEffect } from 'react'
+import { useMap, useMapEvents } from 'react-leaflet'
+import { MarkerData } from './MapComponent'
+
+interface ZoomHandlerProps {
+  markerData: MarkerData | null
+  onZoomEnd: () => void
+}
+
+const ZoomHandler: FC<ZoomHandlerProps> = ({ markerData, onZoomEnd }) => {
+  const map = useMap()
+
+  const flyToMarker = (coordinates: [number, number], zoom: number) => {
+    if (coordinates && typeof coordinates[0] !== 'undefined') {
+      map.flyTo(coordinates, zoom, {
+        animate: true,
+        duration: 3,
+      })
+    }
+  }
+  useMapEvents({
+    zoomend: onZoomEnd,
+  })
+
+  useEffect(() => {
+    if (markerData) {
+      if (
+        markerData.coordinates &&
+        typeof markerData.coordinates[0] !== 'undefined'
+      ) {
+        flyToMarker(markerData.coordinates, 11)
+      }
+    }
+  }, [markerData])
+
+  return null
+}
+
+export default ZoomHandler
