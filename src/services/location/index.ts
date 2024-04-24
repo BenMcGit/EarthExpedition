7b7b4c2b22429175152449a452ded027d581549e
neerajnerlekar <neeraj.nerlekar@gmail.com>
2024-04-23 20:16:33 -0600

    fix the build error. update services-location-index.ts to fix the error

--- src/services/location/index.ts
+++ src/services/location/index.ts
@@ -1,7 +1,7 @@
 import { fetchApi } from '@/utils/fetch';
 
 export const requestGenerateLocation = async (inputPrompt: any) => {
-  const res = await fetchApi({
+  const res: any = await fetchApi({
     path: '/location',
     method: 'POST',
     params: { inputPrompts: inputPrompt },
